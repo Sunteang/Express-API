984cbf1ea91c6858d2ddadee15e46dae4fe8ec64
Sunteang S <sereysunteang@gmail.com>
2024-09-16 10:52:48 +0700

    update some

--- src/utils/constants/app-error-message.ts
+++ src/utils/constants/app-error-message.ts
@@ -6,9 +6,9 @@ export const APP_ERROR_MESSAGE = {
   userReturned: "User Returned successfully",
   userDoesntExist: "User does not exist",
   invalidCredentials: "Invalid user email or password",
-  invalidEmail: "Enter a valid email address",
-  existedEmail: "Email already exists", //here forgot " , "
+  invalidEmail: "Enter a valid email address!",
+  existedEmail: "Email already exists!", //here forgot " , "
   // Product Resource Message Error
-  productDoesntExist: "Product items does not exist!",
+  productDoesntExist: "Product does not exist!",
   // ...
 };
